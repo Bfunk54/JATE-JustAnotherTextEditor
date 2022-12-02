b344946559730ceb8356666ae292608c1fe77c05
Bfunk54 <ben_fein@icloud.com>
2022-12-02 13:43:12 -0500

    starting the install js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,7 +2,14 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    
+    event.preventDefault();
+    
+    window.deferredPrompt = event;
+   
+    butInstall.removeAttribute('hidden');
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});

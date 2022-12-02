e21b855c889831f471140d5e062513b2e850f3f4
Bfunk54 <ben_fein@icloud.com>
2022-12-02 13:57:49 -0500

    finished the install js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -23,4 +23,6 @@ butInstall.addEventListener('click', async () => {
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});

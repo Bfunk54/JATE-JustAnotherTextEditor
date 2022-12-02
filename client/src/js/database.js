c666a5773e168e2e03e23c0db822a0b7a4dbc262
Bfunk54 <ben_fein@icloud.com>
2022-12-02 14:01:49 -0500

    added function to add data to the database in the database js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,22 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Put to the database');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.add({ content: content });
+
+  const result = await request;
+  console.log('Data has beensaved to the database', result);
+}
+ 
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');

617e73689594280565a715776d6af26781a8784b
Bfunk54 <ben_fein@icloud.com>
2022-12-02 13:54:16 -0500

    finished butinstall function in the install js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -12,7 +12,15 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {  
+        const promptEvent = window.deferredPrompt;
+        if (!promptEvent) {
+            return;
+        }
+        promptEvent.prompt();
+        window.deferredPrompt = null;   
+        butInstall.setAttribute('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});

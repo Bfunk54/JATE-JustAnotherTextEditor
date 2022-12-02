6857455e8fb697d0d446311d783878ba6ce05ae4
Bfunk54 <ben_fein@icloud.com>
2022-12-02 14:10:29 -0500

    added function to get all the data from the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -31,6 +31,20 @@ export const putDb = async (content) => {
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();

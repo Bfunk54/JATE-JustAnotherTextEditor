973952966f5319b6c4ac178fd8e8bd7087d637e7
Bfunk54 <ben_fein@icloud.com>
2022-12-02 13:35:49 -0500

    added asset caching to the service worker js

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,12 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  new StaleWhileRevalidate({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+  ],
+}));

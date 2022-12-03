d98c7b440afbd71fb7d0c9f053ae10567f1e39ef
Bfunk54 <ben_fein@icloud.com>
2022-12-02 21:05:39 -0500

    added comments to the index

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,9 +3,12 @@ import Editor from "./editor";
 import "./database";
 import "../css/style.css";
 
+// select the main element
 const main = document.querySelector("#main");
+// set the html of it to nothing to clear it
 main.innerHTML = "";
 
+// function to create and append the spinner to the document
 const loadSpinner = () => {
   const spinner = document.createElement("div");
   spinner.classList.add("spinner");
@@ -17,8 +20,10 @@ const loadSpinner = () => {
   main.appendChild(spinner);
 };
 
+// create a new instance of the editor
 const editor = new Editor();
 
+// if the typeof the editor is undefined, load the spinner
 if (typeof editor === "undefined") {
   loadSpinner();
 }

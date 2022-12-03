d77eaf568e0ae1d08d349923a921e52d35e5db79
Bfunk54 <ben_fein@icloud.com>
2022-12-02 19:30:35 -0500

    fixed styling and removed todos on js files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,50 +1,36 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('Put to the database');
-
-  const jateDb = await openDB('jate', 1);
-
-  const tx = jateDb.transaction('jate', 'readwrite');
-
-  const store = tx.objectStore('jate');
-
+  console.log("Put to the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
   const request = store.add({ content: content });
-
   const result = await request;
-  console.log('Data has beensaved to the database', result);
-}
- 
+  console.log("Data has beensaved to the database", result);
+};
 
-
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('GET from the database');
-
-  const jateDb = await openDB('jate', 1);
-
-  const tx = jateDb.transaction('jate', 'readonly');
-
-  const store = tx.objectStore('jate');
-
+  console.log("GET from the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
   const request = store.getAll();
-
   const result = await request;
-  console.log('result.value', result);
+  console.log("result.value", result);
   return result.value;
-}
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,28 +1,23 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-    
-    event.preventDefault();
-    
-    window.deferredPrompt = event;
-   
-    butInstall.removeAttribute('hidden');
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+
+  window.deferredPrompt = event;
+
+  butInstall.removeAttribute("hidden");
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {  
-        const promptEvent = window.deferredPrompt;
-        if (!promptEvent) {
-            return;
-        }
-        promptEvent.prompt();
-        window.deferredPrompt = null;   
-        butInstall.setAttribute('hidden', true);
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+  butInstall.setAttribute("hidden", true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+window.addEventListener("appinstalled", (event) => {
+  window.deferredPrompt = null;
 });

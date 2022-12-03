72803f24cebac95d50127c86b9a6b98bf9415de7
Bfunk54 <ben_fein@icloud.com>
2022-12-02 20:56:06 -0500

    added comments to the database js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,32 +1,46 @@
 import { openDB } from "idb";
 
+// function to open/create the database
 const initdb = async () =>
+  // open the database
   openDB("jate", 1, {
     upgrade(db) {
+      // if the database already exists, don't create a new one
       if (db.objectStoreNames.contains("jate")) {
         console.log("jate database already exists");
         return;
       }
+      // create the new database
       db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
       console.log("jate database created");
     },
   });
 
+// function to save data to the database
 export const putDb = async (content) => {
   console.log("Put to the database");
+  // open the database
   const jateDb = await openDB("jate", 1);
-  const tx = jateDb.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
+  // create a read/write transaction
+  const transaction = jateDb.transaction("jate", "readwrite");
+  // use the transaction to open the jate object store
+  const store = transaction.objectStore("jate");
+  // add the data to the object store
   const request = store.add({ content: content });
   const result = await request;
   console.log("Data has beensaved to the database", result);
 };
 
+// function to get data from the database
 export const getDb = async () => {
   console.log("GET from the database");
+  // open the database
   const jateDb = await openDB("jate", 1);
-  const tx = jateDb.transaction("jate", "readonly");
-  const store = tx.objectStore("jate");
+  // create a read/write transaction
+  const transaction = jateDb.transaction("jate", "readonly");
+  // use the transaction to open the jate object store
+  const store = transaction.objectStore("jate");
+  // get the data from the object store
   const request = store.getAll();
   const result = await request;
   console.log("result.value", result);
